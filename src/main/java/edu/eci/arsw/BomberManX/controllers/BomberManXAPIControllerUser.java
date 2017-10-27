c88f716d4d619ecca969c5ed9053dd059872c3e6
Sergio:P <dark07perez@gmail.com>
2017-10-27 01:15:01 -0500

    creado formulario registro clases - sin funcionalidad

--- src/main/java/edu/eci/arsw/BomberManX/controllers/BomberManXAPIControllerUser.java
+++ src/main/java/edu/eci/arsw/BomberManX/controllers/BomberManXAPIControllerUser.java
@@ -6,14 +6,12 @@
 package edu.eci.arsw.BomberManX.controllers;
 
 import edu.eci.arsw.BomberManX.services.BomberManXServices;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +49,7 @@ public class BomberManXAPIControllerUser {
         else{
             status =HttpStatus.NOT_FOUND;
         }
-        System.out.println("++++++++++++++++++++++++++++++++++");
+        
         return new ResponseEntity<>(funcion,status);
         
     }   
