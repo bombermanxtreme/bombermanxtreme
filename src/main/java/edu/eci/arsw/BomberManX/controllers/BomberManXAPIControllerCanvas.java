777d48d35ae1babad99b2789002e06b1986d4f12
Kevin S. Sanchez Prieto <kevinssanchez@outlook.com>
2017-11-29 17:03:50 -0500

    Modificaciones mover jugador

---
+++ src/main/java/edu/eci/arsw/BomberManX/controllers/BomberManXAPIControllerCanvas.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.BomberManX.controllers;
+
+import edu.eci.arsw.BomberManX.services.BomberManXServices;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author sergioxl
+ */
+@RestController
+@RequestMapping("/canvas")
+public class BomberManXAPIControllerCanvas {
+
+    @Autowired
+    BomberManXServices gameServices;
+
+    @Autowired
+    SimpMessagingTemplate msmt;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "Ok";
+    }
+
+    private static final Logger LOG = Logger.getLogger(BomberManXAPIControllerUser.class.getName());
+
+    /**
+     *  Permite validar datos de sesion
+     * 
+     * @param correo
+     * @param clave
+     * @return 
+     */
+    @RequestMapping(path = "/{correo}/{clave}", method = RequestMethod.GET)
+    public ResponseEntity<?> loginJugador(@PathVariable String correo, @PathVariable String clave) {
+        HttpStatus status;
+        int id_login = gameServices.loginJugador(correo, clave);
+
+        if (id_login >= 0) {
+            status = HttpStatus.ACCEPTED;
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+        
+        return new ResponseEntity<>(id_login, status);
+    }
+
+    /**
+     * Registra un nuevo jugador
+     * 
+     * @param nombre
+     * @param correo
+     * @param apodo
+     * @param clave
+     * @param imagen
+     * @return ResponseEntity status
+     */
+    @RequestMapping(path = "/new/{nombre}/{correo}/{apodo}/{clave}/{imagen}/", method = RequestMethod.GET)
+    public ResponseEntity<?> RegistrerJugador(@PathVariable String nombre, @PathVariable String correo, @PathVariable String apodo, @PathVariable String clave, @PathVariable String imagen) {
+        HttpStatus status;
+        int id_nuevo = gameServices.registrerJugador(nombre, correo, apodo, clave, imagen);
+
+        if (id_nuevo >= 0) {
+            status = HttpStatus.ACCEPTED;
+        } else {
+            status = HttpStatus.NOT_FOUND;            
+        }
+
+        return new ResponseEntity<>(id_nuevo, status);
+    }
+    
+    /**
+     * Devuelve la imagen url del personaje
+     * 
+     * @param correo
+     * @return ResponseEntity status
+     */
+    @RequestMapping(path = "/avatar/{correo}/view", method = RequestMethod.GET)
+    public ResponseEntity<?> loadAvatar(@PathVariable String correo) {
+        HttpStatus status;
+        String url = gameServices.getUrl(correo);
+        
+        if (!"no_existe".equals(url)) {
+            status = HttpStatus.ACCEPTED;
+        } else {
+            status = HttpStatus.NOT_FOUND;            
+        }
+
+        return new ResponseEntity<>(url, status);
+    }
+
+    /**
+     * Cambia la imagen url del personaje
+     * 
+     * @param correo
+     * @return 
+     */
+    @RequestMapping(path = "/avatar/{correo}/set", method = RequestMethod.GET)
+    public ResponseEntity<?> setAvatar(@PathVariable String correo) {
+        HttpStatus status;
+        String url = gameServices.getUrl(correo);
+        
+        if (!"no_existe".equals(url)) {
+            status = HttpStatus.ACCEPTED;
+        } else {
+            status = HttpStatus.NOT_FOUND;            
+        }
+
+        return new ResponseEntity<>(url, status);
+    }
+}
