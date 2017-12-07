2e4f94d60099c12c91a2b02d9df3ba3ed069acd8
Sergio <dark07perez@gmail.com>
2017-12-07 15:33:46 -0500

    Ajustando imagenes persisnstencia impl jugador

--- src/main/java/edu/eci/arsw/bombermanx/persistencia/impl/PersistenciaImplJugador.java
+++ src/main/java/edu/eci/arsw/bombermanx/persistencia/impl/PersistenciaImplJugador.java
@@ -22,10 +22,10 @@ public class PersistenciaImplJugador implements PersistenciaJugador {
     @Override
     public final void poblar() {
         //necesario para evitar error: overridable method call in constructor//
-        agregarJugador("Kevin Alvarado", "ka@server.com", "Kevincito", "123", "https://vignette.wikia.nocookie.net/looneytunes/images/a/ad/Baby_Lola.png/revision/latest?cb=20110509194052");
+        agregarJugador("Kevin Alvarado", "ka@server.com", "Kevincito", "123", "https://avatarfiles.alphacoders.com/812/81220.jpg");
         agregarJugador("Sergio Pérez", "sp@server.com", "Finanzas", "123", "https://i.pinimg.com/736x/e4/a7/5a/e4a75a1b9ef50e99224830bd2dad8e65--cute-baby-elephant-baby-elephants.jpg");
         agregarJugador("Kevin Sánchez", "ks@server.com", "Quevin", "123", "https://i.pinimg.com/736x/cb/eb/46/cbeb46a7bcde12bea4ff0e7f06b70a03--cartoon-foxes-cartoon-fox-drawing.jpg");
-        agregarJugador("Lina Álvarez", "la@server.com", "Anail", "123", "https://upload.wikimedia.org/wikipedia/commons/d/d3/Bull_cartoon_04.svg");
+        agregarJugador("Lina Álvarez", "la@server.com", "Anail", "123", "https://i.pinimg.com/originals/8c/06/10/8c061085178087b6a063bd6a72b69604.gif");
         agregarJugador("Fanny Pérez", "fp@server.com", "Ynnaf", "123", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYGIRwH_hoWi2hLZ10SR_WkbQ4s35SiRaaVKd-YPXtibOj0-0i-g");
     }
 
