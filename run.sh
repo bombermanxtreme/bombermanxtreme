e237c843e9ec393b0c47fbb2c3cd711db6539ab5
Ubuntu <ubuntu@maquina-1.dd0mql0xph0e1o2aziny4m2dae.jx.internal.cloudapp.net>
2017-12-11 01:12:06 +0000

    quitando abrir navegador de run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 echo "#######################################################################"
 echo "########### Compile, abrir Navegador y subir servidor #################"
 echo "#######################################################################"
-mvn compile && xdg-open http://localhost:8080 && mvn spring-boot:run
+mvn compile && mvn spring-boot:run
